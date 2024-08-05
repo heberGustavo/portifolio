5b2d2598533b2114ed1328f42801c20fc53ee215
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-05 07:34:55 -0300

    Update CSS and create Home page

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import AppRoutes from './routes'
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
+      <AppRoutes />
     </>
   )
 }
---
+++ portfolio/src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+// import styles from "./Home.module.css";
+import Header from "../../components/Header"
+import Container from "../../components/Container"
+import Footer from "../../components/Footer"
+
+function Home() {
+    return(
+        <>
+            <Header />
+            <Container />
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
