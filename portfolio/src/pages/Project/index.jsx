aaafa05602a2bb6be80156641f19f7b789b8e592
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-06 06:50:57 -0300

    Create Card Component

--- portfolio/src/pages/Project/index.jsx
+++ portfolio/src/pages/Project/index.jsx
@@ -1,7 +1,12 @@
+import Card from "../../components/Card";
+
 function Project() {
     return(
         <section>
            <h2>Projeto</h2>
+           <Card />
+           <Card />
+           <Card />
         </section>
     );
 }
