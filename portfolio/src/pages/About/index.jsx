c1a4525d8740650b39129b9629d5755ff4f004b0
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-06 06:41:15 -0300

    Create About Page

--- portfolio/src/pages/About/index.jsx
+++ portfolio/src/pages/About/index.jsx
@@ -1,9 +1,47 @@
-function About() {
-    return(
-        <section>
-            <h2>About</h2>
+import styles from './About.module.css'
+import avatar from './images/avatar.png'
+import html from './images/icon-html.svg'
+import css from './images/icon-css.svg'
+import js from './images/icon-js.svg'
+import react from './images/icon-react.svg'
+import node from './images/icon-node.svg'
+import sql from './images/icon-sql.svg'
+
+function Sobre() {
+    return (
+        <section className={styles.sobre}>
+             
+             <div className={styles.bio}>
+                <img src={avatar} alt="Avatar" className={styles.avatar} />
+                <div className={styles.textos}>
+                    <h1>Sobre</h1>
+
+                    <p>Me chamo <span>Heber Gustavo</span> <br />
+                    <strong>Dev Full Stack</strong> </p>
+
+                    <p>Trabalho com desenvolvimento Web desde 2020.</p>
+
+                    <p>Sou apaixonado por transformar ideias em realidade digital.</p>
+
+                    <p>Especializado em criação de aplicações dinâmicas e intuitivas, <br />
+                    com foco na experiência do usuário.</p>
+                </div>
+            </div>
+
+            <div className={styles.techs}>
+                <h3>Techs</h3>
+                <div className={styles.icones}>
+                    <img src={html} alt="Ícone do html" />
+                    <img src={css} alt="Ícone do css" />
+                    <img src={js} alt="Ícone do js" />
+                    <img src={react} alt="Ícone do react" />
+                    <img src={node} alt="Ícone do node" />
+                    <img src={sql} alt="Ícone do sql" />
+                </div>
+            </div>
+   
         </section>
-    );
+    )
 }
 
-export default About;
+export default Sobre
