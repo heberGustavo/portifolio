31ca70099213c71318906af2a9e35f47f806df29
heberGustavo <heber.gbarbosa@hotmail.com>
2024-08-06 08:11:54 -0300

    Adjustment to make dynamic Cards

--- portfolio/src/pages/Project/index.jsx
+++ portfolio/src/pages/Project/index.jsx
@@ -1,12 +1,43 @@
+import styles from "./Project.module.css";
 import Card from "../../components/Card";
+import { useEffect, useState } from "react";
 
 function Project() {
-    return(
-        <section>
-           <h2>Projeto</h2>
-           <Card />
-           <Card />
-           <Card />
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        const searchRepositories = async () => {
+            const response = await fetch("https://api.github.com/users/heberGustavo/repos");
+            const data = await response.json();
+            setRepositories(data);
+        }
+
+        searchRepositories();
+
+    }, []);
+
+    return (
+        <section className={styles.projetos}>
+            <h2>Projeto</h2>
+            {
+                repositories.length > 0 ? (
+                    <section className={styles.lista}>
+                        {
+                            repositories.map((repo) => (
+                                <Card
+                                    key={repo.id}
+                                    name={repo.name}
+                                    description={repo.description}
+                                    html_url={repo.html_url}
+                                />
+                            ))
+                        }
+                    </section>
+                ) : (
+                    <p>Carregando repositórios</p>
+                )
+            }
         </section>
     );
 }
